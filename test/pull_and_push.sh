0c8c9b065ad381362cbe6726df09b939796175ae
tobe <tobeg3oogle@gmail.com>
2015-12-29 15:10:43 +0800

    Support save images locally

---
+++ test/pull_and_push.sh
@@ -0,0 +1,3 @@
+docker pull centos
+docker tag centos 42.62.101.165:5000/centos
+docker push 42.62.101.165:5000/centos
---
+++ test/pull_and_push_from_alauda.sh
@@ -0,0 +1,3 @@
+docker pull index.alauda.cn/library/nginx
+docker tag index.alauda.cn/library/nginx 42.62.101.165:5000/nginx
+docker push 42.62.101.165:5000/nginx
